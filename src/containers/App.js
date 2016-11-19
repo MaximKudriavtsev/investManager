cdd6f8afa0b96126e3aa57ca5221f8b154a33fbb
maximKudriavtsev <maxim71ruswow@gmail.com>
2016-11-19 03:21:30 +0300

    Added component InfoBox

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,12 +3,14 @@ import React, { Component } from "react"
 import List from "../containers/List"
 import Add from "../components/Add"
 import ChartBox from "../containers/ChartBox"
+import InfoBox from "../components/InfoBox"
 
 import "../styles/app.css"
 
 export default class App extends Component {
     render() {
         return <div className="im">
+            <InfoBox />
             <List />
             <Add />
             <ChartBox />
